e29e3caf60c8dcc53448cf7632f29c4901200fd2
lcarbonn <laurent.carbonnaux@gmail.com>
2025-09-22 09:58:39 +0200

    user id form env in test

--- test/nuxt/user.test.ts
+++ test/nuxt/user.test.ts
@@ -1,23 +1,24 @@
 import { beforeAll, describe, expect, it } from 'vitest'
-import type { ITokenAuth } from '~/types/tokenAuth'
+import type { IBaserowAuth } from '~/types/baserowAuth'
 import { fetchSignInUser } from '~/utils/useFetchAuth'
 
 describe('baserow user', async () => {
-  const login = import.meta.env.VITE_BASEROW_LOGIN
-  const mdp = import.meta.env.VITE_BASEROW_MDP
-  let tokenAuth:ITokenAuth
+  const LOGIN = import.meta.env.VITE_BASEROW_LOGIN
+  const MDP = import.meta.env.VITE_BASEROW_MDP
+  const USER_ID  = import.meta.env.VITE_BASEROW_ID
+  let tokenAuth:IBaserowAuth
 
   beforeAll( async () => {
     // TODO : get env for server
-    tokenAuth = await fetchSignInUser(login, mdp)
+    tokenAuth = await fetchSignInUser(LOGIN, MDP)
   })
 
   // login
   it('login to baserow', async () => {
-    console.log("firstName:", tokenAuth.user.first_name, ", userName", tokenAuth.user.username, ", language", tokenAuth.user.language)
+    console.log("firstName:", tokenAuth.user.first_name, ", userName:", tokenAuth.user.username, ", language:", tokenAuth.user.language, ", id:", tokenAuth.user.id)
     console.log("token:", tokenAuth.token, ", access_token:", tokenAuth.access_token, ", refresh_token:", tokenAuth.refresh_token)
-    expect(tokenAuth.user.username).toEqual(login)
-    expect(tokenAuth.user.id).toEqual(133013)
+    expect(tokenAuth.user.username).toEqual(LOGIN)
+    expect(tokenAuth.user.id).toEqual(Number(USER_ID).valueOf())
 
   })
 })
