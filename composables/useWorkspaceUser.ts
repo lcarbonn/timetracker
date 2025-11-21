fe493de0abd0a230cf63d2235d6f9581f7d3053c
lcarbonn <laurent.carbonnaux@gmail.com>
2025-11-21 11:56:23 +0100

    Update useWorkspaceUser.ts

--- composables/useWorkspaceUser.ts
+++ composables/useWorkspaceUser.ts
@@ -4,7 +4,7 @@
  * @returns Array of users
  */
 export const getWorkspaceUsers = async () :Promise<IWorkspaceUser[]>=> {
-
+  // can only be called on server side du to JWT token
   const result = $fetch<IWorkspaceUser[]>('/api/workspaceusers', {
     onResponseError ({ request, response, options }) {
       // Handle the response errors
