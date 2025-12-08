bdac83a08a169f48feb4b82961e306fc4e784621
lcarbonn <laurent.carbonnaux@gmail.com>
2025-12-08 11:22:26 +0100

    Update utilsFilter.ts

--- app/utils/utilsFilter.ts
+++ app/utils/utilsFilter.ts
@@ -33,19 +33,6 @@ export interface BaserowFilter {
   value: string | number | boolean | null;
 }
 
-export type BaserowFilterGroup = {
-    filter_type: BaserowFilterOperator,
-    filters: BaserowFilter[];
-}
-
-export type BaserowFilters = {
-  filters: {
-    filter_type: BaserowFilterOperator,
-    filters: BaserowFilter[],
-    // groups?: [BaserowFilterGroup]
-  }
-}
-
 export class BaserowFilterBuilder {
   private filters: BaserowFilter[] = [];
 
@@ -58,19 +45,6 @@ export class BaserowFilterBuilder {
       this.filters.push({ field, type, value });
     return this; // chaining
   }
-
-  addIf(
-    condition: boolean,
-    field: string | number,
-    type: BaserowFilterType,
-    value: string | number | boolean | null |undefined
-  ) {
-    if (condition) {
-      this.add(field, type, value);
-    }
-    return this;
-  }
-
   /**
    * Retourne le tableau filters utilisable directement dans la requête Baserow
    */
